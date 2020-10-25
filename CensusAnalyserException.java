5f517721d3da575ca92482566b2212c98e40c519
Ishani9 <57087410+Ishani9@users.noreply.github.com>
2020-10-25 19:01:37 +0530

    Added: Ability to load Census Information from a CSV file

---
+++ CensusAnalyserException.java
@@ -0,0 +1,15 @@
+package gradleAssignment;
+
+@SuppressWarnings("serial")
+public class CensusAnalyserException extends Exception {
+	public enum ExceptionType {
+		CENSUS_FILE_PROBLEM;
+	}
+
+	public ExceptionType type;
+
+	public CensusAnalyserException(String message, ExceptionType type) {
+		super(message);
+		this.type = type;
+	}
+}
