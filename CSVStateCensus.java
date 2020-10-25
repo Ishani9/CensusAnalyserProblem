7ce0415e88dd40e65f80449febf2f8cab44b3d0f
Ishani9 <57087410+Ishani9@users.noreply.github.com>
2020-10-25 17:40:57 +0530

    Welcome to Indian State Census Analyser

---
+++ CSVStateCensus.java
@@ -0,0 +1,7 @@
+package gradleAssignment;
+
+public class CSVStateCensus {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Indian State Census analyser");
+	}
+}
---
+++ StateCensusAnalyser.java
@@ -0,0 +1,7 @@
+package gradleAssignment;
+
+public class StateCensusAnalyser {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Indian State Census analyser");
+	}
+}
